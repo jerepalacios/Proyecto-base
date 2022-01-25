fd828c8fe609efc46efd805e03cd9ed2734067da
Jeremy Palacios <jeremypalaciosmith01@gmail.com>
2022-01-25 19:33:57 -0300

    Proyecto Base

---
+++ Prueba de archivo 2.js
@@ -0,0 +1,18 @@
+let n1 = 18;
+let puedePasar = n1 >= 18 ? "puede pasar" : "tenes q ser mayor"
+
+console.log(puedePasar);
+
+//switch
+let apellido = 'Matinez'
+switch (apellido) {
+    case "Martinez":
+        console.log("tu nombre es Horacio");
+        break;
+    case "Peperoni":
+        console.log('tu nombre es Mario');
+        break;
+    default:
+        console.log('no te tengo en mi base de datos');
+        break;
+}
---
+++ Prueba de archivo.js
@@ -0,0 +1,18 @@
+let n1 = 18;
+let puedePasar = n1 >= 18 ? "puede pasar" : "tenes q ser mayor"
+
+console.log(puedePasar);
+
+//switch
+let apellido = 'Choque'
+switch (apellido) {
+    case "Choque":
+        console.log("tu nombre es franco");
+        break;
+    case "Martorell":
+        console.log('tu nombre es matias');
+        break;
+    default:
+        console.log('no te tengo en mi base de datos');
+        break;
+}
